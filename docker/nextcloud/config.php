7ac4cc7da0cc716c8bc47115c126a1f59637974a
Jordi Burgos <jordiburgos@gmail.com>
2020-03-21 16:55:56 +0000

    Add custom nextcloud configuration

---
+++ docker/nextcloud/config.php
@@ -0,0 +1,34 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'oc73ddck7co9',
+  'passwordsalt' => 'qOvfoVb4elucnPkReRCencoYahaynn',
+  'secret' => 'w6LhCCZIWw9BljP/4ksF9383eSqe9/T8GP/R+7jeKJk4ahxy',
+  'trusted_domains' => 
+  array (
+    0 => 'nextcloud.paellalabs.com',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '18.0.2.2',
+  'overwrite.cli.url' => 'https://nextcloud.paellalabs.com',
+  'overwriteprotocol' => 'https',
+  'overwritehost' => 'nextcloud.paellalabs.com',
+  'installed' => true,
+);
