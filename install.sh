18cf6f4e2549c1361c1010834e4d53c39bf9300f
Jordi Burgos <jordiburgos@gmail.com>
2021-01-17 18:38:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,8 +27,12 @@ git config --global user.email jordiburgos@gmail.com
 
 # NodeJS: https://github.com/nodesource/distributions/blob/master/README.md
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs yarn gcc g++ make
+sudo apt-get install -y nodejs gcc g++ make
 
+# Yarn: https://classic.yarnpkg.com/en/docs/install/#debian-stable
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
 
 # ssh key
 ssh-keygen -t rsa -b 4096 -C "jordiburgos@gmail.com"
